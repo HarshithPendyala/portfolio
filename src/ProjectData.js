6cee71297971c7d2ef91d8a095ce89df5c3a5b4a
Harshith Pendyala <69723975+HarshithPendyala@users.noreply.github.com>
2023-07-17 19:58:08 +0530

    gymate update

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -3,6 +3,16 @@ export const ProjectsData = [
 
     {
         id: 1,
+        img: '/projects/gymate.gif',
+        title:'Gymate',
+        description:'One stop solution for your fitness goals,Gymate provides a space for people to improve their physical fitness and overall health through regular exercise and physical activity. More information on pricing, classes, blog coming soon!',
+        technologies:['React','TailwindCSS','Hooks','Responsive'],  
+        live_demo:'https://gymate-harshith.netlify.app/',
+        code_link:'https://github.com/HarshithPendyala/gymate'     
+    },
+
+    {
+        id: 3,
         img: '/projects/youtube-stats.gif',
         title:'Youtube-Stats',
         description:'Youtube-stats application extracts information and statistics about a Youtube video. It helps Youtube content creaters to find tags of existing videos which are used for Search Engine Optimization.',
@@ -21,19 +31,13 @@ export const ProjectsData = [
         code_link:'https://github.com/HarshithPendyala/linkedin-clone'        
     },
     {
-        id: 3,
+        id: 4,
         img: '/projects/mt.gif',
         title:'Memory Game',
         description:'A fun memory testing game',
         technologies:['HTML','CSS','JavaScript','DOM Manipulation'],
         live_demo:'https://harshith-pendyala-memory-game.netlify.app/',
         code_link:'https://github.com/HarshithPendyala/Memory_Game'        
-    },
-    {
-        id: 4,
-        img: '',
-        title:'Gymate',
-        description:'One stop solution for your fitness goals. COMING SOON!',
-        technologies:['React','TailwindCSS','Hooks'],       
     }
+    
 ];
